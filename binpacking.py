c96948979356ec48585c7a90be6bbbf558ca3f07
Charlotte Desvages <chdesvages@gmail.com>
2023-10-13 11:59:45 +0100

    Finished lecture

--- binpacking.py
+++ binpacking.py
@@ -4,7 +4,33 @@ def first_fit(items, bin_capacity):
     Takes in a list of items of different sizes, and a bin capacity.
     Returns a list of bins and how much they each contain.
     '''
-    pass
+    # Initialise a list of bins
+    bins = []
+
+    # Loop over the list of items
+    for i in items:
+        # Item starts not being placed in a bin
+        placed = False
+
+        # Loop over the bins (until we find a good one)
+        for b in range(len(bins)):
+            # Check bin 0; does i fit?
+            if bin_capacity - bins[b] >= i:
+                # Yes, there is enough space; add current item to the bin
+                bins[b] += i
+                placed = True
+                # Move on to the next item
+                break
+        
+        if not placed:
+        # if placed == False:
+            # Open a new bin
+            bins.append(0)
+            bins[-1] += i
+        
+
+    # Return the result
+    return bins
 
 
 
